ad5cd80edb665c9a16ad60a07e1c18ae82cda93f
Théo mathieu <theo.mathieu@in-progress.io>
2016-06-11 17:35:27 -0400

    Remove AppRegistry, Scene, Router from context

--- src/configs/context.js
+++ src/configs/context.js
@@ -1,6 +1,4 @@
 import Meteor, { Accounts } from 'react-native-meteor';
-import { Scene, Router } from 'react-native-router-flux';
-import { AppRegistry } from 'react-native';
 /*import * as Collections from 'lib/collections';
 
 import { FlowRouter } from 'meteor/kadira:flow-router';
@@ -11,9 +9,6 @@ import { Accounts } from 'meteor/accounts-base';
 export default function () {
   return {
     Meteor,
-    AppRegistry,
-    Scene,
-    Router,
     //FlowRouter,
     //Collections,
     //LocalState: new ReactiveDict(),
--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { AppRegistry, Navigator } from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
 
 import Home from './containers/home';
 
-export default function(injectDeps, { AppRegistry, Scene, Router }) {
+export default function(injectDeps) {
 
   const App = () => (
-    <Router>
+    <Router sceneStyle={{paddingTop: Navigator.NavigationBar.Styles.General.TotalNavHeight}}>
       <Scene key="root">
         <Scene key="home" component={Home} title="Home"/>
       </Scene>
